2a33d30291930a775b9dd78bb13044819e5d889e
Claire Shen <claire@Claires-MacBook-Air.local>
2023-01-27 13:59:39 -0800

    add notes

--- first_app/forms.py
+++ first_app/forms.py
@@ -8,26 +8,28 @@ def check_for_z(value):
         raise forms.ValidationError("NAME NEEDS TO START WITH Z")
 
 
-class FormName(forms.Form):
-    # name = forms.CharField(validators=[check_for_z])
-    name = forms.CharField()
-    email = forms.EmailField()
-    verify_email = forms.EmailField(label='Enter your email again:')
-    text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False,
-                                widget=forms.HiddenInput,
-                                validators=[validators.MaxLengthValidator(0)])
+# class FormName(forms.Form):
+#     # name = forms.CharField(validators=[check_for_z])
+#     name = forms.CharField()
+#     email = forms.EmailField()
+#     verify_email = forms.EmailField(label='Enter your email again:')
+#     text = forms.CharField(widget=forms.Textarea)
+#     botcatcher = forms.CharField(required=False,
+#                                 widget=forms.HiddenInput,
+#                                 validators=[validators.MaxLengthValidator(0)])
+#
+#
+#     def clean(self):
+#         all_clean_data = super().clean()
+#         email = all_clean_data['email']
+#         vmail = all_clean_data['verify_email']
+#
+#         if email != vmail:
+#             raise forms.ValidationError("Make Sure Email Match!")
 
-
-    def clean(self):
-        all_clean_data = super().clean()
-        email = all_clean_data['email']
-        vmail = all_clean_data['verify_email']
-
-        if email != vmail:
-            raise forms.ValidationError("Make Sure Email Match!")
-
-
+# ModelForm used to accept input and pass it to a model.
+# Here we import User model from models.py and create NewUserForm to get input and
+# pass it to DB User model using Meta()
 class NewUserForm(forms.ModelForm):
     class Meta():
         model = User
